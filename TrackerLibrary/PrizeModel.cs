e4cb5edd7e8438acb5d399167579d32d75f80c9c
Joel-Milligan <52927756+Joel-Milligan@users.noreply.github.com>
2021-05-27 22:23:57 +0800

    Create prize constructor for validating parameters

--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -32,5 +32,21 @@ namespace TrackerLibrary
         /// The percent of prize pool that this prize awards.
         /// </summary>
         public double PrizePercent { get; set; }
+
+        public PrizeModel() {}
+
+        public PrizeModel(string placeName, string placeNumber, string prizeAmount, string prizePercentage)
+        {
+            PlaceName = placeName;
+
+            int.TryParse(placeNumber, out int placeNumberValue);
+            PlaceNumber = placeNumberValue;
+
+            decimal.TryParse(prizeAmount, out decimal prizeAmountValue);
+            PrizeAmount = prizeAmountValue;
+
+            double.TryParse(prizePercentage, out double prizePercentageValue);
+            PrizePercent = prizePercentageValue;
+        }
     }
 }
