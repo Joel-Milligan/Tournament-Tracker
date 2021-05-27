a07989ca121c2f8b6f3c6438ee5fc8bc0a86832d
Joel-Milligan <52927756+Joel-Milligan@users.noreply.github.com>
2021-05-27 21:19:25 +0800

    Added a base global config static class

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; } = new();
+
+        public static void InitConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // TODO: Create the SQL connection
+            }
+
+            if (textFiles)
+            {
+                // TODO: Create the text connection
+            }
+        }
+    }
+}
