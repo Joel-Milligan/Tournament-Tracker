2134773932506418938817cf37b3759e7ea80f80
Joel-Milligan <52927756+Joel-Milligan@users.noreply.github.com>
2021-05-28 21:49:59 +0800

    Implemented creating a prize in sql database

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -1,10 +1,11 @@
-﻿using TrackerLibrary.Models;
+﻿using Dapper;
+using System.Data;
+using TrackerLibrary.Models;
 
 namespace TrackerLibrary.DataAccess
 {
     public class SqlConnector : IDataConnection
     {
-        // TODO: Make the CreatePrize method actually save to the database.
         /// <summary>
         /// Saves a new prize to the database
         /// </summary>
@@ -12,7 +13,18 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information, including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            using IDbConnection connection = new Microsoft.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments"));
+            DynamicParameters p = new();
+
+            p.Add("@PlaceNumber", model.PlaceNumber);
+            p.Add("@PlaceName", model.PlaceName);
+            p.Add("@PrizeAmount", model.PrizeAmount);
+            p.Add("@PrizePercentage", model.PrizePercent);
+            p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+            connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+            model.Id = p.Get<int>("@id");
 
             return model;
         }
